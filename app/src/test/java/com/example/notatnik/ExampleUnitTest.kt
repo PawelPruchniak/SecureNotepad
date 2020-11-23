33c736f77adbf1fea84a858b3862452d80e54891
PawelPruchniak <pw.pruchniak@gmail.com>
2020-11-23 22:03:33 +0100

    Adding check_encrypt_if_password_is_wrong test

--- app/src/test/java/com/example/notatnik/ExampleUnitTest.kt
+++ app/src/test/java/com/example/notatnik/ExampleUnitTest.kt
@@ -1,8 +1,10 @@
 package com.example.notatnik
 
+import com.example.notatnik.screens.security.Encryption
+import org.junit.Assert
 import org.junit.Test
-
-import org.junit.Assert.*
+import java.text.DateFormat
+import java.util.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +13,26 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun check_encrypt_if_password_is_wrong() {
+        val pass = "hasło123"
+        var password = CharArray(pass.length)
+        password = pass.toCharArray()
+
+        val pass2 = "haslo123"
+        var password2 = CharArray(pass2.length)
+        password2 = pass2.toCharArray()
+
+        val currentDateTimeString = DateFormat.getDateTimeInstance().format(Date())
+        val password_encrypted = Encryption().encrypt(currentDateTimeString.toByteArray(Charsets.UTF_8), password)
+
+        println("password encrypted: $password_encrypted")
+
+        val password_decrypted = Encryption().decrypt(password_encrypted, password2)
+
+        println("password decrypted: $password_decrypted")
+
+        Assert.assertNotNull("Verify password_decrypted is not null", password_encrypted)
+        Assert.assertNull("Verify password_decrypted is null", password_decrypted)
+
     }
 }
