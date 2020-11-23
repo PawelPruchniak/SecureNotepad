be3aac8871d719d4fdb30cee974145671a5f7596
PawelPruchniak <pw.pruchniak@gmail.com>
2020-11-23 22:08:51 +0100

    Adding check_encrypt_if_password_is_correct test, and cosmetic changes of tests

--- app/src/test/java/com/example/notatnik/ExampleUnitTest.kt
+++ app/src/test/java/com/example/notatnik/ExampleUnitTest.kt
@@ -14,25 +14,34 @@ import java.util.*
 class ExampleUnitTest {
     @Test
     fun check_encrypt_if_password_is_wrong() {
-        val pass = "hasło123"
-        var password = CharArray(pass.length)
-        password = pass.toCharArray()
-
-        val pass2 = "haslo123"
-        var password2 = CharArray(pass2.length)
-        password2 = pass2.toCharArray()
+        val passwordFirst = "hasło123".toCharArray()
+        val passwordSecond = "haslo123".toCharArray()
 
         val currentDateTimeString = DateFormat.getDateTimeInstance().format(Date())
-        val password_encrypted = Encryption().encrypt(currentDateTimeString.toByteArray(Charsets.UTF_8), password)
+        val passwordEncrypted = Encryption().encrypt(currentDateTimeString.toByteArray(Charsets.UTF_8), passwordFirst)
+        println("password encrypted: $passwordEncrypted")
 
-        println("password encrypted: $password_encrypted")
+        val passwordDecrypted = Encryption().decrypt(passwordEncrypted, passwordSecond)
+        println("password decrypted: $passwordDecrypted")
 
-        val password_decrypted = Encryption().decrypt(password_encrypted, password2)
+        Assert.assertNotNull("Verify password encrypted is not null", passwordEncrypted)
+        Assert.assertNull("Verify password decrypted is null", passwordDecrypted)
+    }
 
-        println("password decrypted: $password_decrypted")
+    @Test
+    fun check_encrypt_if_password_is_correct() {
+        val passwordFirst = "hasło123".toCharArray()
+        val passwordSecond = "hasło123".toCharArray()
 
-        Assert.assertNotNull("Verify password_decrypted is not null", password_encrypted)
-        Assert.assertNull("Verify password_decrypted is null", password_decrypted)
+        val currentDateTimeString = DateFormat.getDateTimeInstance().format(Date())
+        val passwordEncrypted = Encryption().encrypt(currentDateTimeString.toByteArray(Charsets.UTF_8), passwordFirst)
+        println("password encrypted: $passwordEncrypted")
+
+        val passwordDecrypted = Encryption().decrypt(passwordEncrypted, passwordSecond)
+        println("password decrypted: $passwordDecrypted")
+
+        Assert.assertNotNull("Verify password encrypted is not null", passwordEncrypted)
+        Assert.assertNotNull("Verify password decrypted is not null", passwordDecrypted)
 
     }
 }
